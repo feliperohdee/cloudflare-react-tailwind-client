0d3d14cde67defff3cdba18da2bdb947f8b33eba
Felipe Rohde <feliperohdee@gmail.com>
2025-11-20 13:37:12 -0300

    Refactor i18n integration in main.tsx to utilize i18nLoader for language support and loading; remove deprecated useI18n hook to streamline the codebase.

--- app/main.tsx
+++ app/main.tsx
@@ -6,16 +6,16 @@ import { Toastr } from 'use-toastr';
 
 import App from '@/app';
 import cookies from '@/app/libs/cookies';
-import i18n from '@/i18n';
+import i18nLoader from '@/i18n/loader';
 
 (() => {
 	const lang = (() => {
 		const lang = cookies.get('lang');
 
-		return i18n.supports(lang) ? lang : 'en-us';
+		return i18nLoader.supports(lang) ? lang : 'en-us';
 	})();
 
-	i18n.load(lang);
+	i18nLoader.load(lang);
 	createRoot(document.getElementById('root')!).render(
 		<StrictMode>
 			<App />
