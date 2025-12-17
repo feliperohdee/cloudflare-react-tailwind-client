4b9424851934b848a65a7f05d068bbdf02f84c68
Felipe Rohde <feliperohdee@gmail.com>
2025-12-17 15:54:03 -0300

    Update internationalization hook to utilize new browserI18nLoader; adjust language handling to align with updated I18n structure for improved consistency and functionality.

--- app/hooks/use-i18n.ts
+++ app/hooks/use-i18n.ts
@@ -1,11 +1,11 @@
 import { useCallback } from 'react';
 
-import i18nLoader, { SupportedLang } from '@/i18n/loader';
+import { browserI18nLoader, I18n } from '@/i18n';
 
 const useLang = () => {
-	const { lang } = i18nLoader.store;
+	const { lang } = browserI18nLoader.store;
 
-	const setLang = useCallback((lang: SupportedLang) => {
+	const setLang = useCallback((lang: I18n.SupportedLang) => {
 		const url = new URL(window.location.href);
 		url.searchParams.set('lang', lang);
 
